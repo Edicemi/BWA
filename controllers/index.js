e5abb7fcbf5bfd53ff7bda4b9ee98820cd667712
Edicemi <victoriataiwo1998@gmail.com>
2022-06-30 15:10:50 +0100

    account created

--- controllers/index.js
+++ controllers/index.js
@@ -1,52 +1,52 @@
 // //generate 10 random numbers
-// const randomNumber = () => {
-//     return Math.floor(Math.random() * 10);
-// }
+
 
 
 // //return error is number is less than 500
-// const error = (number) => {
-//     if (number < 500) {
-//         return 'error';
-//     }
-// }
 
-const path = require("path");
 const Account = require("../models/account");
 const CustomError = require("../lib/error");
 const responseHandler = require("../utils/responseHandler");
 const { validationResult, body } = require("express-validator");
 const { passwordHash, passwordCompare } = require("../lib/bcrypt");
 const { jwtSign } = require("../lib/ath");
-const { uploadCloudinary } = require("../lib/cloudinary");
 
-const DatauriParser = require("datauri/parser");
-const parser = new DatauriParser();
 
-exports.signUp = async (req, res, next) => {
-    const { fullname, email, password } = req.body;
+exports.create_account = async (req, res, next) => {
+    const { accountname, deposit, password, accountnumber } = req.body;
     try {
         const result = validationResult(req);
         if (!result.isEmpty()) {
             throw new CustomError().check_input();
         }
-        if (fullname && email && password) {
-            let userExist = await User.findOne({ email: email });
+        if (accountname && deposit && password && accountnumber) {
+            let userExist = await Account.findOne({ accountname: accountname });
             if (userExist) {
                 throw new CustomError(
-                    `Email ${email} already exist, try another one.`,
+                    `Accountname ${accountname} already exist, try another one.`,
                     400
                 );
             }
+            const myNumber = () => {
+                return Math.floor(Math.random() * 10);
+            };
+
+            const deposit = (number) => {
+                if (number < 500) {
+                    return 'error';
+                }
+            };
+
             const hashedPassword = await passwordHash(password);
             const user = new User({
-                fullname: fullname,
-                email: email,
+                accountname: accountname,
+                deposit: deposit,
                 password: hashedPassword,
+                accountnumber: myNumber,
             });
 
-            await user.save();
-            return responseHandler(res, 201, "User account created succesfully.");
+    await user.save();
+            return responseHandler(res, 200, "User account created succesfully.");
 
         } else {
             throw new CustomError().invalid_parameter();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+router = express.Router();
+const { body } = require("express-validator");
+const {
+  create_account,
+  login,
+  reviewPost,
+  reviewCount,
+  fetchHigestCountAggregate
+} = require("../controllers/index");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const { validateUserToken } = require("../lib/ath");
+
+// route for authentication
+router.post(
+  "/create_account",
+  body("accountname", "Name is required").trim(),
+  body("password", "Password must be of  8 characters long and alphanumeric")
+    .trim()
+    .isLength({ min: 8 })
+    .isAlphanumeric(),
+  create_account
+);
+
+router.post("/login", login);
+router.post("/review", validateUserToken, reviewPost);
+router.put("/countAdd/:reviewId", reviewCount);
+router.get("/getReview", fetchHigestCountAggregate);
 
 module.exports = router;
+
