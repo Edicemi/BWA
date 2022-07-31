558e01c0a6e80b28b8a6d9f3f804f0951b9f6dc9
Edicemi <victoriataiwo1998@gmail.com>
2022-07-31 10:13:12 +0100

    algorithm

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-const public = "victoria is in a meeting with tobi";
-public[0].toUpperCase();
 
-console.log(public);
+// capitalize first letter of each word
+function capitalizeFirstLetter(string) {
+    return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+console.log(capitalizeFirstLetter('foo bar bag'));
+
+
+
+
